80a96abf5c66ccbe1c55178f58e0738e9eb196e9
pizzaconhojas <rzeznikludwik@gmail.com>
2026-05-16 14:27:55 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,20 +7,46 @@ const wpisane_haslo = document.getElementById("pass1")
 function sprawdz_haslo() {
     const haslo = document.getElementById("pass1").value;
 
-    if(haslo == "antek") {
-        alert('niepotrzebny alert');
+    if(haslo == "PNCONTRACTEXECUTIONIMMINENT") {
+        //strona od Marcina Z
         location.href="./podstrona.html";
     }
 
-    else if(haslo == "haslo") {
-        //alert('ndaegt');
+    else if(haslo == "F1R3em0J1") {
+        //strona od Zuzi Ż
         location.href="./podstrona1.html";
     }
 
-    else if(haslo == "maslo") {
-        //alert('ndaegt');
+    else if(haslo == "LOD69") {
+        //strona od Kingi
         location.href="./podstrona2.html";
     }
+
+    else if(haslo == "e=mc^2") {
+        location.href="./film_kostki.html"
+    }
+
+    else if(haslo == "diva") {
+        location.href="./film_panek.html"
+    }
+
+    else if(haslo == "Banan") {
+        location.href="./film_mariny.html"
+    }
+
+    else if(haslo == "s3Mi-ducK") {
+        location.href="./film_antka.html"
+    }
+
+    else if(haslo == "PioXDenaturat18") {
+        location.href="./film_janka.html"
+    }
+
+     else if(haslo == "") {
+        //strona ode mnie
+        alert("okej to dziala")
+    }
+
 }
 
 function pokaz_alert_z_dupy() {
